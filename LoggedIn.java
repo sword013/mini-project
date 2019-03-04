3cdd90a7f8fd307c0cc26c396aa0ef15d756ef68
Tejas Deshmukh <47255298+sword013@users.noreply.github.com>
2019-03-04 20:14:42 +0530

    Create LoggedIn.java

---
+++ LoggedIn.java
@@ -0,0 +1,16 @@
+package com.example.hello;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class LoggedIn extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_logged_in);
+
+
+    }
+}
